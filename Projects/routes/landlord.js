cdaab69be5fdbbc6b629cb7da8d89a69fca15edb
Nati_Tedros <tedros.natnael@gmail.com>
2022-02-14 02:44:30 +0300

    Added

--- Projects/routes/landlord.js
+++ Projects/routes/landlord.js
@@ -4,7 +4,39 @@ const authentication = require('../middleware/authentication.js');
 
 const router = Router();
 
+//Landlord Home Page
+router.get('/landlord/home' , (req , res) => {
+    const sql = "select * from landlord right join houses on landlord.idLandlord = houses.HouseNo where houses.Owner = 1";
+    db.query(sql,[req.params.id] , (err , result) => {
+        
+        if (err) {
+            console.log("error quering sql");
+        }
+        console.log(result);
+        res.render('./LandLord/landlordHome.ejs' , {data : result} );
+    })
+});
 
+//Add House
+router.get('/addhouse' , (req , res) => {
 
+    res.render('./LandLord/addHouse.ejs' );
 
+});
+
+router.post('/landlord/addhouse' , (req , res) => {
+
+    console.log(req.body);
+    let sql = "INSERT INTO houses values ('" + req.body.houseNo + "' , '" + req.body.location + "' , '" + req.body.price + "' , '" + req.body.date + "' , '" + req.body.availibility + "' , '" + req.body.owner + "')";
+    db.query(sql , (err , result) => {
+        if(err){
+            console.log("error quering sql");
+        }
+       res.redirect('/landlord/home' );
+   });
+   
+
+});
+
+//
 module.exports = router;
--- Projects/routes/tenant.js
+++ Projects/routes/tenant.js
@@ -4,7 +4,23 @@ const authentication = require('../middleware/authentication.js');
 
 const router = Router();
 
+//Home Page for Tenant
+router.get("/tenant/home",  authentication.isAdminLoggedIn   , (req, res) => {
+    res.render("Tenant/tenantHome", { error: false, error2: false});
+});
 
+//Popular Houses List
+router.get('/popular' , (req , res) => {
+    const sql = "select houses.* from houses left join houserating on houses.HouseNo = houserating.idHouseRating where houserating.Rating >= 3 ";
+    db.query(sql , (err , result) => {
+        
+        if (err) {
+            console.log("error quering sql");
+        }
+        console.log(result);
+        res.render('popular' , {data : result} );
+    })
+});
 
 
 module.exports = router;
